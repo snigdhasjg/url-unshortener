b43f55d864c9de330f09c9020c6422b1bf42ba29
Snigdhajyoti Ghosh <snigdhasjg@users.noreply.github.com>
2026-09-19 23:32:21 +0800

    initial generated code
    created via command: quarkus create app com.snigji:unshortener:1.0.0-SNAPSHOT --package-name=com.snigji.unshortener --java=25 --gradle-kotlin-dsl -x rest-jackson,vertx,cache,smallrye-health --no-code

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    java
+    id("io.quarkus")
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+dependencies {
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+    implementation("io.quarkus:quarkus-rest-jackson")
+    implementation("io.quarkus:quarkus-smallrye-health")
+    implementation("io.quarkus:quarkus-cache")
+    implementation("io.quarkus:quarkus-vertx")
+    implementation("io.quarkus:quarkus-arc")
+    testImplementation("io.quarkus:quarkus-junit")
+}
+
+group = "com.snigji"
+version = "1.0.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_25
+    targetCompatibility = JavaVersion.VERSION_25
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
